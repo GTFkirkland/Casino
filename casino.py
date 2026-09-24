1518a67aa20c4cb39a523b08d88675b2ac049eb2
GTFkirkland <mk536875@georgetownisd.org>
2026-09-24 09:05:00 -0500

    Update casino.py

--- casino.py
+++ casino.py
@@ -1,6 +1,18 @@
 import random as ran
-#intro
+#start function
 def intro():
-    print("~~WELCOME To THE CASINO~~")
-    game = input("""Which game will you play?
-                """)
+    game = 0
+    print("""-----------------
+Welcome to casino
+-----------------
+Which game will you play?
+1) Blackjack""")
+
+    while not game > 0 and not game <= 1:
+        try:
+            game = int(input("<answer>> "))
+        except ValueError:
+            print(f"""{game} is not a valid answer
+                  Try again""")
+
+intro()
