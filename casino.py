b950e1bd8202d46d08391928f6391ea50a8225ff
GTFkirkland <mk536875@georgetownisd.org>
2026-09-24 10:34:48 -0500

    Update casino.py

--- casino.py
+++ casino.py
@@ -1,18 +1,26 @@
 import random as ran
-#start function
-def intro():
+money = 200
+
+#game selection
+def gameSelection():
+    global gameLibrary
+    global game
     game = 0
-    print("""-----------------
-Welcome to casino
------------------
+    gameLibrary = ["1"]
+    print("""--------------------
 Which game will you play?
 1) Blackjack""")
-
-    while not game > 0 and not game <= 1:
-        try:
-            game = int(input("<answer>> "))
-        except ValueError:
+    while not game in gameLibrary:
+        game = input("<answer>> ")
+        if not game in gameLibrary:
             print(f"""{game} is not a valid answer
-                  Try again""")
+Try again""")
 
-intro()
+#games
+def playGame(): 
+    None
+#start
+print("""-----------------
+Welcome to casino""")
+gameSelection()
+playGame()
